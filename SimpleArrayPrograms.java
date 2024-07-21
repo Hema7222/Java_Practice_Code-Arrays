cefd26c2a034124fdfb09dca5f58607b074e0edb
Hema7222 <hemajeya004@gmail.com>
2024-07-21 21:45:41 +0530

    Simple Array

---
+++ SimpleArrayPrograms.java
@@ -0,0 +1,18 @@
+package Java.practice.array;
+
+
+/*---------------Sum of all elements in an array of integers----------------*/
+
+public class SimpleArrayPrograms {
+
+	public static void main(String[] args) {
+		int sum=0;
+		int arr[] = {10, 20, 30,40,50};
+		for(int i=0;i<arr.length; i++) {
+			sum=sum+arr[i];
+		}
+		System.out.println("Sum: "+sum);
+
+	}
+
+}
