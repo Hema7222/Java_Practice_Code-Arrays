f6bafa6c525a8a82d4f1d54df0056ba7c0682466
Hema7222 <hemajeya004@gmail.com>
2024-07-22 16:11:19 +0530

    Simple Arrays Program

--- SimpleArrayPrograms.java
+++ SimpleArrayPrograms.java
@@ -20,32 +20,52 @@ package Java.practice.array;
 
 /*--------------------Reverse an array--------------------*/
 
-import java.util.Scanner;
+//import java.util.Scanner;
+//
+//public class SimpleArrayPrograms {
+//
+//	public static void main(String[] args) {
+////		int arr[] = {10, 20, 30,40,50};
+//		Scanner s = new Scanner(System.in);
+//		System.out.println("Enter the number of elements in an array: ");
+//		int n = s.nextInt();
+//		
+//		int arr[] = new int[n];
+//		System.out.println("Enter the elements: ");
+//		for(int i=0;i<n;i++) {
+//			arr[i]=s.nextInt();
+//		}
+//		
+//		System.out.print("[");
+//		for(int i=arr.length-1;i>=0; i--) {
+//			System.out.print(arr[i]);
+//			if(i!=0) {
+//				System.out.print(", ");
+//			}
+//		}
+//		System.out.print("]");
+//
+//		s.close();
+//	}
+//
+//}
+
+
+
+/*--------------------Average of all elements in an array--------------------*/
 
 public class SimpleArrayPrograms {
 
 	public static void main(String[] args) {
-//		int arr[] = {10, 20, 30,40,50};
-		Scanner s = new Scanner(System.in);
-		System.out.println("Enter the number of elements in an array: ");
-		int n = s.nextInt();
-		
-		int arr[] = new int[n];
-		System.out.println("Enter the elements: ");
-		for(int i=0;i<n;i++) {
-			arr[i]=s.nextInt();
+		int sum=0;
+		int arr[] = {95, 92, 97, 86, 98};
+		for(int i=0;i<arr.length; i++) {
+			sum=sum+arr[i];
 		}
-		
-		System.out.print("[");
-		for(int i=arr.length-1;i>=0; i--) {
-			System.out.print(arr[i]);
-			if(i!=0) {
-				System.out.print(", ");
-			}
-		}
-		System.out.print("]");
+		float Avg = (float)sum/arr.length;
+		System.out.println("Sum: "+sum);
+		System.out.println("Avg: "+Avg);
 
-		s.close();
 	}
 
 }
